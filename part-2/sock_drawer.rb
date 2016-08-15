1917c1372d566e896d152bf2c831c59cfd700297
khamla719 <khamla719@yahoo.com>
2016-08-15 11:55:31 -0400

    Add method to supply match from sockdrawer

--- part-2/sock_drawer.rb
+++ part-2/sock_drawer.rb
@@ -5,4 +5,13 @@ class SockDrawer
     @socks = args.fetch(:socks) { Array.new }
     @matcher = args.fetch(:matcher)
   end
+
+  def supply_match_for(possible_match)
+    # iterate through all socks in the drawer
+    # compare each sock to possible_match
+    # if matching_attributes, return both socks
+    # remove those socks from drawer
+    matched_sock = @socks.find {|s| @matcher.match?(s, possible_match)}
+    @socks.delete(matched_sock)
+  end
 end
